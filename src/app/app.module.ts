19629e988f1e7b689d9385c3f4b8866242fdee1d
Steven Escobar Castaño <stevenlikeplis@gmail.com>
2019-11-16 20:01:28 -0500

    cambios de estilo a la master page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './public/masterPage/nav-bar/nav-bar.component';
 import { FooterComponent } from './public/masterPage/footer/footer.component';
 import { HeroComponent } from './public/masterPage/hero/hero.component';
 import { NavbarComponent } from './public/masterPage/navbar/navbar.component';
@@ -11,7 +9,6 @@ import { NavbarComponent } from './public/masterPage/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     FooterComponent,
     HeroComponent,
     NavbarComponent
